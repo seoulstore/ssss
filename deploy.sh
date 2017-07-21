db314228ea1b6bb0c549c101080ecce8678ef3d2
scjang <wkdtjdcjf294@gmail.com>
2017-07-21 16:41:50 +0900

    qwer

--- deploy.sh
+++ deploy.sh
@@ -31,7 +31,7 @@ git config user.email $GIT_USER_EMAIL || exit 1
 
 git add -A . || exit 1
 git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
-git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
+git push --force "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages
 
 echo "Cleaning up temp files"
 rm -Rf $DIST_DIRECTORY
