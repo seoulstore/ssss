5cc150ad16dc60c5285f4a6a55dbdcaa324c7518
scjang <wkdtjdcjf294@gmail.com>
2017-07-21 15:44:22 +0900

    adfadsfasdf

--- deploy.sh
+++ deploy.sh
@@ -10,23 +10,22 @@ DIST_DIRECTORY="_gh_pages/"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
 ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
-GIT_USER_NAME=`git config user.name`
-GIT_USER_EMAIL=`git config user.email`
+GIT_USER_NAME="scjang"
+GIT_USER_EMAIL="wkdtjdcjf294@gmail.com"
 
 cp .gitignore $DIST_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
 git checkout -B gh-pages || exit 1
 
-echo "Removing old static content"
-git rm -rf $DIST_DIRECTORY || exit 1
+#echo "Removing old static content"
+#git rm -rf $DIST_DIRECTORY || exit 1
 
 #echo "Copying dist content to root"
 #cp -r $DIST_DIRECTORY/ . || exit 1
 #cp $DIST_DIRECTORY.gitignore . || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
-echo "$GIT_USER_NAME"
 git config user.name $GIT_USER_NAME || exit 1
 git config user.email $GIT_USER_EMAIL || exit 1
 
