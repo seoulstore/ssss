d4142a47bf35a4d587966339da2d0b834a28a976
scjang <wkdtjdcjf294@gmail.com>
2017-07-23 21:52:56 +0900

    test #47

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 echo "**********************************   Starting deployment   **********************************"
 echo "********************************** Target: gh-pages branch **********************************"
 
-travis encrypt GH_TOKEN="9db57a607ef5c2f6df783875b913ae5fa0e9bfc7" --add
+#travis encrypt GH_TOKEN="9db57a607ef5c2f6df783875b913ae5fa0e9bfc7" --add
 
+GH_TOKEN="ZmSlRw9CVWWdcp5hRfR91qxSGUPkQ1/fYhw3MW6OIcQi397XA1S6dAUIdSlQz4sA6SdNkgsWERkCsLXeAJ1q4muXy8tNYIK50IuevDm2eoL9w4mLGM8Ld+z8Kdx0d0iVAogvaSBTbh+eiN+QTjvV3s32S/hszFVx3o3gM6lOLyVC0Mvrz7BeJj8hFJZzg/zgLmZUzATzk/wooqro/B725jPjR0jK5ztMI5kxyn+v7Aby+e+0rDUVveHHW0tT9whgxvObTG5mXfvN22zBMKRoT0ucIQJD2ZP1g7qY55StgcHIfLCVx459VAKAkMCjqx+3sfBmd4kXHZuZVer7vJVkjdvCsQWqpLExIHSn80Ba6pVKXlsM0JSrnXDxKDQl98FuRGbirCEeXQsmFoF1yywXBp9mTVkP6Pg8wgFN0fao9xgLfIBlGcRPnmeoT+2bxwYeW5gkFt95h+R1kper6iLPiJrlwxYNFJpJ6wneuCXljN/kz+p2HrGXzpYdCw9qaZWjJhYbAmYYb+Ykiug3OAPH5xfhKJSJJb53EzuGzlR1iSLIOTsofwba6genApBlycTv8xOUiX+PYUh8YvdYwHIOrLTfolDj9jy2V63pnWnFmi5wWP/8QSLSCAr3oiHDqhjZBbw3OLd4mG4Tg3AZCQpCKn9xmzSX+c6b2ce+7dGex7Q="
 DIST_DIRECTORY="_gh_pages/"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
