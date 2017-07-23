1ce3beaf699c6e24b6d48bb9a7a00ca830679cf4
scjang <wkdtjdcjf294@gmail.com>
2017-07-23 21:48:13 +0900

    test #46

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,32 @@
-#!/bin/bash
-#echo "**********************************   Starting deployment   **********************************"
-#echo "********************************** Target: gh-pages branch **********************************"
+!/bin/bash
+echo "**********************************   Starting deployment   **********************************"
+echo "********************************** Target: gh-pages branch **********************************"
 
-#travis encrypt GH_TOKEN="9db57a607ef5c2f6df783875b913ae5fa0e9bfc7" --add
+travis encrypt GH_TOKEN="9db57a607ef5c2f6df783875b913ae5fa0e9bfc7" --add
 
-#DIST_DIRECTORY="_gh_pages/"
-#CURRENT_COMMIT=`git rev-parse HEAD`
-#ORIGIN_URL=`git config --get remote.origin.url`
-#ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GH_TOKEN@github.com}
-#GIT_USER_NAME="scjang"
-#GIT_USER_EMAIL="wkdtjdcjf294@gmail.com"
+DIST_DIRECTORY="_gh_pages/"
+CURRENT_COMMIT=`git rev-parse HEAD`
+ORIGIN_URL=`git config --get remote.origin.url`
+ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GH_TOKEN@github.com}
+GIT_USER_NAME="scjang"
+GIT_USER_EMAIL="wkdtjdcjf294@gmail.com"
 
-#cp .gitignore $DIST_DIRECTORY || exit 1
+cp .gitignore $DIST_DIRECTORY || exit 1
 
-#echo "Checking out gh-pages branch"
-#git checkout -B gh-pages || exit 1
+echo "Checking out gh-pages branch"
+git checkout -B gh-pages || exit 1
 
-#echo "Pushing new content to $ORIGIN_URL"
-#git remote -v
-#git config user.name $GIT_USER_NAME || exit 1
-#git config user.email $GIT_USER_EMAIL || exit 1
+echo "Pushing new content to $ORIGIN_URL"
+git remote -v
+git config user.name $GIT_USER_NAME || exit 1
+git config user.email $GIT_USER_EMAIL || exit 1
 
-#git add -A . || exit 1
-#git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
-#git push --force "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages
+git add -A . || exit 1
+git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
+git push --force "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages
 
-#echo "Cleaning up temp files"
-#rm -Rf $DIST_DIRECTORY
+echo "Cleaning up temp files"
+rm -Rf $DIST_DIRECTORY
 
-#echo "Deployed successfully."
-#exit 0
+echo "Deployed successfully."
+exit 0
