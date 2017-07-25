562cb440038e518ece1a6e1567055376aa81bb64
scjang <wkdtjdcjf294@gmail.com>
2017-07-25 21:51:37 +0900

    test #100

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,9 @@ git checkout -B gh-pages || exit 1
 
 echo "Remove"
 ls
+echo ""
 #find ./ ! -name $DIST_DIRECTORY -exec rm -rf {} \;
+find ./ -name $DIST_DIRECTORY -type f -ls
 
 echo "Pushing new content to $ORIGIN_URL"
 git add -A . || exit 1
