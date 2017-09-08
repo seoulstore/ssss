cd64c1793bed532a201cf5829094afaa6d67e033
scjang <wkdtjdcjf294@gmail.com>
2017-09-08 18:00:15 +0900

    deploy shell script modify 2

--- deploy.sh
+++ deploy.sh
@@ -13,18 +13,26 @@ echo "Checking out gh-pages branch"
 git checkout -B gh-pages || exit 1
 
 echo "Remove"
-#find ./ ! -name _gh_pages -exec rm -rf {} \;
-rm -rf bower.json CHANGELOG.md composer.json _config.yml CONTRIBUTING.md deploy.sh fonts Gemfile Gemfile.lock grunt Gruntfile.js .hound.yml ISSUE_TEMPLATE.md js npm-shrinkwrap.json nuget package.js package.json package-lock.json .sass-cache docs
-cp -a _gh_pages/* ./
-rm -rf _gh_pages
+rm -rf \
+bower.json CHANGELOG.md composer.json _config.yml \
+CONTRIBUTING.md deploy.sh fonts Gemfile Gemfile.lock \
+grunt Gruntfile.js .hound.yml ISSUE_TEMPLATE.md js \
+npm-shrinkwrap.json nuget package.js package.json \
+package-lock.json .sass-cache docs \
+scss .editorconfig .eslintrc.json \
+.gitattributes .gitignore .scss-lint.yml \
+.travis.yml Gulpfile.js LICENSE README.md || exit 1
+
+cp -a _gh_pages/* ./ || exit 1
+rm -rf _gh_pages || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git add -A . || exit 1
 git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
-git push --force "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages
+git push --force "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages || exit 1
 
 echo "Cleaning up temp files"
-rm -Rf $DIST_DIRECTORY
+rm -Rf $DIST_DIRECTORY || exit 1
 
 echo "Deployed successfully."
 exit 0
