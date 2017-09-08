392f4d8f80c8b91f2a6b6c06742d1a3aeeefd136
scjang <wkdtjdcjf294@gmail.com>
2017-09-08 17:49:45 +0900

    deploy shell script modify

--- deploy.sh
+++ deploy.sh
@@ -6,27 +6,19 @@ DIST_DIRECTORY="_gh_pages/"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
 ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GH_TOKEN@github.com}
-GIT_USER_NAME="scjang"
-GIT_USER_EMAIL="wkdtjdcjf294@gmail.com"
 
 cp .gitignore $DIST_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
 git checkout -B gh-pages || exit 1
 
-echo ""
-ls
-echo ""
-git branch
-echo ""
-cd _gh_pages
-echo ""
-ls
+echo "Remove"
+#find ./ ! -name _gh_pages -exec rm -rf {} \;
+rm -rf bower.json CHANGELOG.md composer.json _config.yml CONTRIBUTING.md deploy.sh fonts Gemfile Gemfile.lock grunt Gruntfile.js .hound.yml ISSUE_TEMPLATE.md js npm-shrinkwrap.json nuget package.js package.json package-lock.json .sass-cache docs
+cp -a _gh_pages/* ./
+rm -rf _gh_pages
 
 echo "Pushing new content to $ORIGIN_URL"
-git config user.name $GIT_USER_NAME || exit 1
-git config user.email $GIT_USER_EMAIL || exit 1
-
 git add -A . || exit 1
 git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
 git push --force "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages
